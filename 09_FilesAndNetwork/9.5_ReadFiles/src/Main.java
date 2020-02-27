8fc95239d8c0abc45968971052b47be1cb1058cf
Atarikir <mitkiril@gmail.com>
2020-02-27 12:56:48 +0300

    change gitignore and ReadFiles

--- 09_FilesAndNetwork/9.5_ReadFiles/src/Main.java
+++ 09_FilesAndNetwork/9.5_ReadFiles/src/Main.java
@@ -1,6 +1,4 @@
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -8,9 +6,8 @@ import java.nio.file.Paths;
 import java.text.DecimalFormat;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
         for (; ; ) {
             System.out.println("Enter the path to the folder whose size you want to receive in the format:\n" +
                     "\"C:/doc/file.txt\"");
@@ -24,16 +21,23 @@ public class Main {
                         .mapToLong(p -> p.toFile().length())
                         .sum();
 
-                String[] units = new String[]{"B", "KB", "MB", "GB", "TB"};
-                int unitIndex = (int) (Math.log10(size) / 3);
-                double unitValue = 1 << (unitIndex * 10);
+                bytesToReadable(size);
 
-                String readableSize = new DecimalFormat("#,##0.#")
-                        .format(size / unitValue) + " " + units[unitIndex];
-                System.out.println(readableSize);
             } catch (Exception ex) {
                 System.out.println("Wrong path to file or folder!");
             }
         }
+    }
+
+    private static void bytesToReadable(long size) {
+
+        String[] units = new String[]{"B", "KB", "MB", "GB", "TB"};
+        int unitIndex = (int) (Math.log10(size) / 3);
+        double unitValue = 1 << (unitIndex * 10);
+
+        String readableSize = new DecimalFormat("#,##0.#")
+                .format(size / unitValue) + " " + units[unitIndex];
+
+        System.out.println(readableSize);
     }
 }
