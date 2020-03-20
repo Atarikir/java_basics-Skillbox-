c604956d584f0fd776101f5fa40a4c92a1693541
Pandora <mitkiril@gmail.com>
2020-03-21 00:08:24 +0300

    ParsingBankStatement completion2

--- 09_FilesAndNetwork/9.9_ParsingBankStatement/src/Main.java
+++ 09_FilesAndNetwork/9.9_ParsingBankStatement/src/Main.java
@@ -1,26 +1,80 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
+
+    private static String transactionFile = "09_FilesAndNetwork/files/movementList.csv";
 
     public static void main(String[] args) {
 
-        Transaction transaction = new Transaction();
+        List<Transaction> listOfTransaction = parseLine();
 
-        //460800,00
-        System.out.printf("%n%s%,.2f%s%n", "Поступление денежных средств на счет : ", transaction.parseLineAmount()
+        //460800,00 доходы
+        System.out.printf("%n%s%d%n", "Поступление денежных средств на счет : ", listOfTransaction
                 .stream()
-                .mapToDouble(Double::parseDouble)
-                .sum(), " руб.");
+                .mapToLong(Transaction::getIncome)
+                .sum());
 
         System.out.println("-----------------------------------------------------------");
         System.out.println("Расходы по контрагентам : " + "\n");
 
-        transaction.parseLineByCategory()
-                .forEach((k, v) -> System.out.printf("%-32s - %,.2f руб.%n", k, v));
+        listOfTransaction.stream()
+                .collect(Collectors.groupingBy(Transaction::getContractor,
+                        Collectors.summingLong(Transaction::getExpense)))
+                .forEach((k, v) -> System.out.printf("%-32s - %d%n", k, v));
 
         System.out.println("------------------------------------------------------------");
-        //466393,07
-        System.out.printf("%n%-32s - %,.2f%s%n", "Денежные расходы по счету ", transaction.parseLineExpense()
+
+        //466393,07 расходы
+        System.out.printf("%n%-32s - %d%n", "Денежные расходы по счету ", listOfTransaction
                 .stream()
-                .mapToDouble(Double::parseDouble)
-                .sum(), " руб.");
+                .mapToLong(Transaction::getExpense)
+                .sum());
+    }
+
+    private static List<Transaction> parseLine() {
+
+        List<Transaction> listOfTransaction = new ArrayList<>();
+
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(transactionFile));
+            lines.remove(0);
+
+            for (String line : lines) {
+
+                String[] fragments = line.replace("\"", "").split(",", 8);
+
+                if (fragments.length != 8) {
+                    System.out.println("Wrong line: " + line);
+                    continue;
+                }
+
+                if (fragments[7].matches(".+,.{2}")) {
+                    fragments[7] = fragments[7].replace(",", "");
+                } else if (fragments[7].matches(".+,.")) {
+                    fragments[7] = fragments[7].replace(",", "").concat("0");
+                } else fragments[7] = fragments[7].concat("00");
+
+                fragments[5] = fragments[5].substring(20, 60);
+                if (fragments[5].startsWith("\\")) {
+                    fragments[5] = fragments[5].substring(1).replaceFirst("GBR\\\\", "").trim();
+                } else {
+                    fragments[5] = fragments[5].substring(12).replaceFirst("\\\\", "").trim();
+                }
+
+                listOfTransaction.add(new Transaction(
+                        Long.parseLong(fragments[6].concat("00")),
+                        Long.parseLong(fragments[7]),
+                        fragments[5]
+                ));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return listOfTransaction;
     }
 }
--- 09_FilesAndNetwork/9.9_ParsingBankStatement/src/Transaction.java
+++ 09_FilesAndNetwork/9.9_ParsingBankStatement/src/Transaction.java
@@ -1,74 +1,24 @@
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
-
 public class Transaction {
 
-    private List<String> amount;
-    private List<String> expense;
-    private Map<String, Double> counterpart;
+    private long income;
+    private long expense;
+    private String contractor;
 
-    private static List<String> lines;
-
-    static {
-        try {
-            lines = Files.readAllLines(Paths.get("09_FilesAndNetwork/files/movementList.csv"));
-            lines.remove(0);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public Transaction(long income, long expense, String contractor) {
+        this.income = income;
+        this.expense = expense;
+        this.contractor = contractor;
     }
 
-    public List<String> parseLineAmount() {
-
-        amount = new ArrayList<>();
-
-        for (String line : lines) {
-            String[] fragments = line.split(",");
-            amount.add(fragments[6]);
-        }
-        return amount;
+    public long getIncome() {
+        return income;
     }
 
-    public List<String> parseLineExpense() {
-
-        expense = new ArrayList<>();
-
-        for (String line : lines) {
-            line = line.replace("\"", "");
-            String[] fragments = line.split(",", 8);
-            expense.add(fragments[7].replace(",", "."));
-        }
+    public long getExpense() {
         return expense;
     }
 
-    public Map<String, Double> parseLineByCategory() {
-
-        counterpart = new TreeMap<>();
-
-        for (String line : lines) {
-            line = line.replace("\"", "");
-            String[] fragments = line.split(",", 8);
-            String key = fragments[5].substring(20, 60);
-            if (key.startsWith("\\")) {
-                key = key.substring(1).replaceFirst("GBR\\\\", "").trim();
-            } else {
-                key = key.substring(12).replaceFirst("\\\\", "").trim();
-            }
-            Double value = Double.parseDouble(fragments[7].replace(",", "."));
-
-            if (counterpart.containsKey(key)) {
-                value = value + counterpart.get(key);
-                counterpart.put(key, value);
-            }
-            if (!counterpart.containsKey(key)) {
-                counterpart.put(key, value);
-            }
-        }
-        return counterpart;
+    public String getContractor() {
+        return contractor;
     }
 }
