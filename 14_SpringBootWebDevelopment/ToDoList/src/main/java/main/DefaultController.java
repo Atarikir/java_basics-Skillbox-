545eb42be789fc3746613673e4e9d359ebc46ee6
pandora <mitkiril@gmail.com>
2021-05-19 22:38:45 +0300

    add database ToDoList

--- 14_SpringBootWebDevelopment/ToDoList/src/main/java/main/DefaultController.java
+++ 14_SpringBootWebDevelopment/ToDoList/src/main/java/main/DefaultController.java
@@ -1,16 +1,33 @@
 package main;
 
+import main.model.Task;
+import main.model.TaskRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Date;
+import java.util.ArrayList;
 
-@RestController
+@Controller
 public class DefaultController {
 
+    private TaskRepository taskRepository;
+
+    public DefaultController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
     @RequestMapping("/")
-    public String index() {
-        //return (new Date()).toString();
-        return String.valueOf((int) (1 + 100 * Math.random()));
+    public String index(Model model) {
+
+        Iterable<Task> taskIterable = taskRepository.findAll();
+        ArrayList<Task> tasks = new ArrayList<>();
+        for(Task task : taskIterable) {
+            tasks.add(task);
+        }
+
+        model.addAttribute("tasks", tasks);
+        model.addAttribute("taskCount", tasks.size());
+        return "index";
     }
 }
---
+++ 14_SpringBootWebDevelopment/ToDoList/src/main/java/main/TaskController.java
@@ -0,0 +1,73 @@
+package main;
+
+import main.model.Task;
+import main.model.TaskRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class TaskController {
+
+    private final TaskRepository taskRepository;
+
+    public TaskController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    @GetMapping("/tasks/")
+    public List<Task> list() {
+        Iterable taskIterable = taskRepository.findAll();
+
+        List<Task> tasks = new ArrayList<>();
+        for (Object task : taskIterable) {
+            tasks.add((Task) task);
+        }
+        return tasks;
+    }
+
+    @PostMapping("/tasks/")
+    public int add(Task task) {
+        Task newTask = taskRepository.save(task);
+        return newTask.getId();
+    }
+
+    @GetMapping("/tasks/{id}")
+    public ResponseEntity<?> get(@PathVariable int id) {
+        Optional optionalTask = taskRepository.findById(id);
+        if (!optionalTask.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        return new ResponseEntity<>(optionalTask.get(), HttpStatus.OK);
+    }
+
+    @PutMapping("/tasks/{id}")
+    public ResponseEntity<?> save(@PathVariable int id, Task newTask) {
+        Optional<Task> optionalTask = taskRepository.findById(id);
+        if (!optionalTask.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        taskRepository.save(newTask);
+        return new ResponseEntity<>(newTask, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/tasks/{id}")
+    public ResponseEntity<?> deleteId(@PathVariable int id) {
+        Optional optionalTask = taskRepository.findById(id);
+        if (!optionalTask.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+        taskRepository.deleteById(id);
+        return new ResponseEntity<>(optionalTask.get(), HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/tasks/")
+    public void deleteAll() {
+        taskRepository.deleteAll();
+    }
+}
